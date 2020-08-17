953d2c8f5dd521aacc118ecade2b7e3243d889cf
Chukwuebuka Ifechukwu <ife_ebuka@ymail.com>
2020-08-17 17:22:06 +0100

    Explain more about project in ReadMe Add class and functions to get_store_info.py

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# info_collection
-# A mini questionnaire will be presented for the user to answer
-# Answers will be collected and stored in a file
-An excel sheet will the be created
+# Project 1
+This project involves creating a python script which will do the following:
+- Provide a mini questionnaire which will be presented for the user to answer
+- collate all answers of a each user to a file
+- create an excel sheet and export file to the sheet
+- sort information in file into tables
+
+Information now put in table form can then be further analysed in excel background
+
+Further practice: analysis of information can be done in python with the help of pandas and numpy
--- get_store_info.py
+++ get_store_info.py
@@ -1,21 +1,59 @@
-#from datetime import datetime
+import json
 
-class Test_Results:
-    def __init__(self, name, surname, age):
+
+class Get_Info:
+    def __init__(self, name, surname, age,serial):
         self.name = name
         self.surname = surname
         self.age = age
+        self.answers = [self.name,self.surname,self.age]
+    
+    #function will make file for storing answers
+    def create_file(self):
+        self.file = open("info_file.txt", "a+")
 
-    def Create_Info_File(self):
-        full_name = self.name + self.surname
-        file = open(full_name, "w")
-        file.write(self.name + " " + self.surname + " is ready to proceed with what needs to be done")
+    #function states questions and obtains answers
+    def q_and_a(self):
+        self.answers.append(input("What month were you born in: "))
+        self.answers.append(input("What's your favourite colour: "))
+        self.answers.append(input("what job are you in: "))
+        self.answers.append(input("which state are you living in: ")) 
+        self.make_dict()
+        
+    def make_dict(self):
+        self.info_dict = {}
+        self.info_dict[1] = self.answers
 
-    def Add_info(self):
+    #function will store the answers in the file
+    def store_answers(self):
+        with open('info_file.txt', "a") as outfile:
+            json.dump(self.info_dict, outfile)
+
+
+class Send_To_Excel:
+    def __init__(self):
+        pass
+ 
+    def open_excel(self):
         pass
 
+    def make_table(self):
+        pass
 
-print("Testing program")
-test = Test_Results("Ebuka","Ifechukwu", 23)
-test.Create_Info_File()
-print("Program testing complete")
+    def fill_table(self):
+        pass
+
+print("Welcome welcome")
+number = int(input("what is your number: "))
+name = input("Please state your name: ")
+surname = input("Now state your surname: ")
+age = int(input("How old are you: "))
+
+test = Get_Info(name,surname,age)
+test.create_file()
+test.q_and_a()
+print(test.answers)
+test.store_answers()
+with open('info_file.txt') as json_file:
+    data = json.load(json_file)
+    print(data)
